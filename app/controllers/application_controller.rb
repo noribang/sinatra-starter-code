fa7eb83a8b4aff4c3fc3bab34675b470eddfc7a3
Tom Tobar <cocdetombomb@gmail.com>
2022-08-04 22:47:21 -0500

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < Sinatra::Base
+
+end
+
+# This controller will be the "entry point" to our Sinatra 
+# app. All other controllers will inherit from ApplicationController
+
